b4dffba52df79bea4edab81865fea40367daabd6
John Doe <119933836+PrimalTea@users.noreply.github.com>
2022-12-06 11:25:38 +0100

    Update sender.py
    J'ai ajouter une fonctionnalité essentielle au fonctionnement du logiciel

--- sender.py
+++ sender.py
@@ -1,11 +1,1 @@
-import socket
-
-hote = "localhost"
-port = 5566
-
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect((hote, port))
-print("Connection on {}".format(port))
-
-socket.send("Hey my name is Olivier!".encode())
-
+print("hello world")
