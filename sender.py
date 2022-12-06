62d10cb64992050fc5679dc710a74ab8fdaa64a2
John Doe <119933836+PrimalTea@users.noreply.github.com>
2022-12-06 11:47:34 +0100

    Update sender.py
    Début du menu

--- sender.py
+++ sender.py
@@ -1,1 +1,19 @@
-print("hello world")
+# Importation
+
+
+# Ici on ouvre le fichier de donnée
+fichier_donne = open('data.txt', 'r+')
+
+
+print("""Bonjour Bienvenue dans votre repertoire téléphonique !\n
+0- Quitter
+1- Ecrire dans le répertoire
+2- Rechercher dans le répertoire
+""")
+# ici on créer une boucle infini afin d'effectuer pour une interface utilisateur simple
+while True:
+  ask = input("Quel est votre choix ? ")
+  # Arret de la boucle si le choix est 0
+  if ask == "0":
+    print("Merci d'avoir utilisé RepPhone, au revoir !")
+    break
