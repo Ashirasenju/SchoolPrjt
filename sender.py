2e8eed64f8f69ece15dc33c09bb91fe126741c89
PrimalTea <louret2006bis@gmail.com>
2022-12-07 15:54:42 +0100

    J'ai créé le fihier data.txt / J'ai quasiment fini le choix 1 (ajout de contact à la liste data du répertoire) / Il faut que tu termines le choix 2

--- sender.py
+++ sender.py
@@ -1,21 +1,19 @@
-# Importation
+# Importation UI
 
-# Definition des fonctions
-def addUser():
-    pass
-  
+
+
+
+
+# Exécution du choix 2
 def searchUser():
   pass
 # Ici on ouvre le fichier de donnée
 fichier_donnee = open('data.txt', 'r+')
 # On créé un tableau vide nommé 'data'
-data = []
-# # parcourir chaques valeurs du fichier data.txt ; les séparer par des virgules ; les mettre dans un dictionnaire nommé dict et ajouté chaques dictionnaires à la liste data.
-for i in fichier_donnee:
-  nom,prenom,num = i.split(",")
-  dict = {"nom" : nom, "prenom": prenom, "num": num}
-  data.append(dict)
-print(data)
+
+# parcourir chaques valeurs du fichier data.txt ; les séparer par des virgules ; les mettre dans un dictionnaire nommé dict et ajouté chaques dictionnaires à la liste data.
+
+
 # Création du menu des choix
 print("""Bonjour Bienvenue dans votre repertoire téléphonique !\n
 0- Quitter
@@ -25,13 +23,32 @@ print("""Bonjour Bienvenue dans votre repertoire téléphonique !\n
 # ici on créer une boucle infini afin d'effectuer pour une interface utilisateur simple
 # la commande necéssaire s'active selon le choix de l'utilisateur
 while True:
+  data=[]
   ask = input("Quel est votre choix ? ")
   # Arret de la boucle si le choix est 0
   if ask == "0":
     print("Merci d'avoir utilisé RepPhone, au revoir !")
     break
-  if ask == "1":
+  elif ask == "1":
+    def addUser():
+      askprenom=input('Entrez le prénom du nouveau contact :  ')
+      asknom=input('Entrez le nom du nouveau contact :  ')
+      asknum=input('Entrez le numéro du nouveau contact :  ')
+      dict["nom":asknom, "prenom":askprenom, "num":asknum]
+      data.append(dict)
+      for x in data:
+        print(x)
     addUser()
+    break
+  
+  
   elif ask == "2":
-    searchUser()
-#coucou
+    for i in fichier_donnee:
+      nom,prenom,num = i.split(",")
+      dict = {"nom" : nom, "prenom": prenom, "num": num}
+      data.append(dict)
+      print(data)
+
+  else:
+    print("Ce choix n'existe pas")
+
