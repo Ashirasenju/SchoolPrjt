257138f21e8445af21271afb192146df7a115199
John Doe <119933836+PrimalTea@users.noreply.github.com>
2022-12-07 14:24:00 +0100

    Update sender.py
    création des commentaires avec les #

--- sender.py
+++ sender.py
@@ -8,19 +8,22 @@ def searchUser():
   pass
 # Ici on ouvre le fichier de donnée
 fichier_donnee = open('data.txt', 'r+')
+# On créé un tableau vide nommé 'data'
 data = []
+# # parcourir chaques valeurs du fichier data.txt ; les séparer par des virgules ; les mettre dans un dictionnaire nommé dict et ajouté chaques dictionnaires à la liste data.
 for i in fichier_donnee:
   nom,prenom,num = i.split(",")
   dict = {"nom" : nom, "prenom": prenom, "num": num}
   data.append(dict)
 print(data)
-
+# Création du menu des choix
 print("""Bonjour Bienvenue dans votre repertoire téléphonique !\n
 0- Quitter
 1- Ecrire dans le répertoire
 2- Rechercher dans le répertoire
 """)
 # ici on créer une boucle infini afin d'effectuer pour une interface utilisateur simple
+# la commande necéssaire s'active selon le choix de l'utilisateur
 while True:
   ask = input("Quel est votre choix ? ")
   # Arret de la boucle si le choix est 0
