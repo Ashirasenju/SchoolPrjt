9723f3c8e3715899263387a7c57fe331602513eb
PrimalTea <louret2006bis@gmail.com>
2022-12-07 16:31:49 +0100

    J'ai créé le fichier data.txt ; J'ai enlever les commentaire, on les fera à la fin J'ai changé la forme globale du script pour que ce soit plus compréhensible J'ai commencé (et je pense même avoir fini) le choix n°1 Il faut que tu termines le choix n°2

--- sender.py
+++ sender.py
@@ -1,52 +1,34 @@
-# Importation UI
-
-
-
-
-# Definition des fonctions
-def addUser():
-    pass
-  
-def searchUser():
-  pass
-# Ici on ouvre le fichier de donnée
-fichier_donnee = open('data.txt', 'r+')
-# On créé un tableau vide nommé 'data'
-data = []
-# # parcourir chaques valeurs du fichier data.txt ; les séparer par des virgules ; les mettre dans un dictionnaire nommé dict et ajouté chaques dictionnaires à la liste data.
-for i in fichier_donnee:
-  nom,prenom,num = i.split(",")
-  dict = {"nom" : nom, "prenom": prenom, "num": num}
-  data.append(dict)
-print(data)
-# Création du menu des choix
 print("""Bonjour Bienvenue dans votre repertoire téléphonique !\n
 0- Quitter
 1- Ecrire dans le répertoire
 2- Rechercher dans le répertoire
 """)
-# ici on créer une boucle infini afin d'effectuer pour une interface utilisateur simple
-# la commande necéssaire s'active selon le choix de l'utilisateur
+fichier_donnee = open('data.txt', 'r+')
+data=[]
+for i in fichier_donnee:
+    prenom,nom,num = i.split(",")
+    dict={"nom":nom, "prenom":prenom, "num":num}
+    data.append(dict)
+print(data)
+
 while True:
-  data=[]
+  
   ask = input("Quel est votre choix ? ")
-  # Arret de la boucle si le choix est 0
+
   if ask == "0":
     print("Merci d'avoir utilisé RepPhone, au revoir !")
     break
+  
   elif ask == "1":
     def addUser():
       askprenom=input('Entrez le prénom du nouveau contact :  ')
       asknom=input('Entrez le nom du nouveau contact :  ')
       asknum=input('Entrez le numéro du nouveau contact :  ')
-      dict["nom":asknom, "prenom":askprenom, "num":asknum]
-      data.append(dict)
-      for x in data:
-        print(x)
+      dict1={}
+      dict1["nom1"]=asknom
+      dict1["prenom1"]=askprenom
+      dict1["num1"]=asknum
+      data.append(dict1)
+      print(data)
     addUser()
     break
-  
-  
-  elif ask == "2":
-    searchUser()
-#coucou
