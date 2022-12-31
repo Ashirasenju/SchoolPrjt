a2e3e97d85326bad95c84d9ea2363bb29d6777fc
Ashirasenju <elspameurdel93@gmail.com>
2022-12-31 14:25:31 +0100

    prettyListUser() est fonctionnel Ajout de commentaire

--- sender.py
+++ sender.py
@@ -4,6 +4,7 @@ print("""Bonjour Bienvenue dans votre repertoire téléphonique !\n
 0- Quitter
 1- Ecrire dans le répertoire
 2- Rechercher dans le répertoire
+3- Lister les contacts
 """)
 data=[]
 #ouverture du fichier data.txt correspondant au repertoire telephonique 
@@ -11,6 +12,7 @@ fichier_donnee = open('data.txt', 'r+')
 
 #creation de la fonction permettant d'ajouter un contact au repertoire telephonique
 def addUser():
+      # Demande des données à travèrs des inputs
       askprenom=input('Entrez le prénom du nouveau contact :  ')
       asknom=input('Entrez le nom du nouveau contact :  ')
       asknum=input('Entrez le numéro du nouveau contact :  ')
@@ -18,7 +20,9 @@ def addUser():
       dict1["nom"]=asknom
       dict1["prenom"]=askprenom
       dict1["num"]=asknum
+      # Assignation des données au dictionnaire
       data.append(dict1)
+      # Ecriture des données sur le fichier
       fichier_donnee.write(asknom + "," + askprenom + "," + asknum + "\n")
       print("Utilisateur Ajouté(e)")
 for i in fichier_donnee:
@@ -28,7 +32,7 @@ for i in fichier_donnee:
 
 #creation de la fonction permettant de rechercher un contact et d'en ajouter si le contact n'existe pas
 def searchUser(nom):
-      # ici les .lower() serve a rendre insensible à la casse(donc ca ne tient pas compte des majuscules et minuscules )
+      # ici les .lower() servent à rendre insensible à la casse(donc ca ne tient pas compte des majuscules et minuscules )
       for i in data:
             if i["nom"].lower() == nom.lower():
                   print(i["num"])
@@ -37,15 +41,19 @@ def searchUser(nom):
                   print(i["num"])
                   break
             else:
-                  ask_2 = input("Se nom n'existe pas... Voulez vous l'ajouter ?")
+                  ask_2 = input("Ce nom n'existe pas... Voulez vous l'ajouter ?")
                   if ask_2 == "oui" or "Oui" or "y":
                         addUser()
                         break
                   else:
                         break
-#creation d'une fonction supplementaire permettant simplement de voir la liste de contact 
+# Creation d'une fonction supplementaire permettant simplement de voir la liste de contact de manière agréable.
 def prettyListUser():
-      pass
+      for i in data:
+            name = i["nom"]
+            first_name = i["prenom"]
+            Numero = i["num"]
+            print(f"{name} {first_name} -> {num}")      
 
 while True:
   
